3a0cd64adffcfaa49109f1bb32ebbafd19676875
Khalid Mesbah <89298608+khalidMesbah@users.noreply.github.com>
2022-01-20 19:33:58 +0200

    Update merge sort.js

--- sorting algorithms/merge sort.js
+++ sorting algorithms/merge sort.js
@@ -1,1 +1,13 @@
-kj
+let l1 = [1, 3, 5, 7];
+let l2 = [0, 2, 4];
+
+function mergeSort(l1, l2) {
+  let l = [];
+
+  while (l1.length && l2.length)
+    l1[0] < l2[0] ? l.push(l1.shift()) : l.push(l2.shift());
+
+  return [...l, ...l1, ...l2];
+}
+
+console.log(mergeSort(l1, l2));
